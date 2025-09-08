66b06e51d930569df800f91062a1904e42517db8
Laksh1104 <laksh.boodnah@mail.utoronto.ca>
2025-09-08 19:14:59 -0400

    Overloading multiples

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,17 +2,21 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
-        System.out.println("Total number of non-negative multiples: " + multiples(3,5,100));
+        System.out.println("Total number of non-negative multiples: " + multiples());
     }
 
      public static int multiples(int n, int a, int b) {
         int count = 0;
         for (int i = 1; i < n; i++) {
             if (i % a == 0 || i % b == 0) {
-                count += 1;
+                count ++;
             }
         }
         return count;
+    }
+
+    public static int multiples(){
+        return multiples(1000,3,5);
     }
 }
 
