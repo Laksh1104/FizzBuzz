2b7580d25e697bec9e79dc4da4ac365875351c75
Laksh1104 <laksh.boodnah@mail.utoronto.ca>
2025-09-09 12:23:18 -0400

    Bonus task: Creating reduce to 0 function

---
+++ src/lab1/Reduce.java
@@ -0,0 +1,23 @@
+package lab1;
+
+public class Reduce {
+    public static void main(String[] args) {
+        int n = 100;
+        System.out.println(reduce(n));
+    }
+
+    public static int reduce(int n) {
+        int count = 0;
+
+        while (n != 0) {
+            if (n % 2 == 0) {
+                n = n / 2;
+            }
+            else {
+                n = n - 1;
+            }
+            count++;
+        }
+        return count;
+    }
+}
